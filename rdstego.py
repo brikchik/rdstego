556a66dd465a02d76b7376e11f7d76e55fa8678f
Gleb Rudometov <javist.gr@gmail.com>
2018-05-06 22:11:53 +0500

    python and ffmpeg only from now on
    works under any platform

--- rdstego.py
+++ rdstego.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-# rdstego - video steganography tool
+# rdstego - video steganography tool (any video supported by FFMpeg -> mp4 [H264/MP3])
 # author: Gleb Rudometov
 # email: javist.gr@mail.ru
-# have to be configuredon both sides to hide and extract successfully:
+# have to be configured on both sides to hide and extract successfully (and make analysis harder):
 OVERALL_CTR_VALUE='1111111111111111'    # cipher iv
 INNER_CTR_VALUE='0000000000000000'      # cipher iv
-KEY_LENGTH=128 # AES key size (up to 128 bits). decrease it if video is way too short
+KEY_LENGTH=12 # AES key size (up to 128 bits). Decrease it if video is way too short.
 ADD_BLINKING=False # should we change the frames?
-SEPARATOR='@#@#' # required for successful extraction
+SEPARATOR='@#@#' # required for successful extraction.
+SIGNATURE='&F%H' # signature of stegged frame in audio file. You can make it dynamic to make analysis harder yourself.
 # extra settings
 SHOW_VIDEO=False #show video while processing it?
 SHOW_EXTRACTED_MESSAGE=False # show extracted data?
@@ -23,7 +24,126 @@ import zlib
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from PIL import Image, ImageDraw
-def deldir(dirname, create=False): # remove or create directory with content
+LENGTH=8 # header(7 bytes for SIGNATURE and data size(3 byte number) + at least 1 free byte for data)
+bmpeg1=((32, 32, 32), (40, 48, 64), (48, 56, 96),
+        (56, 64, 128), (64, 80, 160), (80, 96, 192),
+        (96, 112, 224), (112, 128, 256), (128, 160, 288),
+        (160, 192, 320), (192, 224, 352), (224, 256, 384),
+        (256, 320, 416), (320, 384, 448))
+samplingRate = (44100, 48000, 32000)
+samplingPerFrame = (1152, 1152, 384)
+def ReadFile(filename): #returns string
+    music_file=open(filename, 'rb')
+    content=music_file.read()
+    return(content)
+def WriteFile(outfilename, content):#gets list
+    out_file=open(outfilename, 'wb')
+    out_file.write(''.join(content))
+def FindFrame(content, headerPos=0, header=''):
+    FIND_NEXT = headerPos!=0
+    if FIND_NEXT: pos=headerPos+FrameSize(header)
+    else: pos=0
+    while pos<len(content)-3: # while is faster than for
+        headerArray=content[pos:pos+4]
+        if FIND_NEXT:
+            if ord(headerArray[0])==0xFF and (ord(headerArray[1]) & 0xE0)==0xE0 and (ord(headerArray[2]) & 0xF0)!=0xF0:
+                return pos, headerArray
+        elif headerArray[0]==chr(0xFF) and (headerArray[1]==chr(0xFB) or ord(headerArray[1])==0xFA):
+            return pos, headerArray                                
+        pos+=1
+    return -1, ''
+def FrameSize(headerArray): # parsing frame header
+    LayerIdx=(ord(headerArray[1]) >> 1) & 0x3 - 1
+    BitrateIdx=(ord(headerArray[2]) >> 4) & 0xF - 1
+    SamplingRateIdx=(ord(headerArray[2]) >> 2) & 0x3;
+    Padding=(ord(headerArray[2]) >> 1) & 0x1;
+    size=(samplingPerFrame[LayerIdx] / 8 * bmpeg1[BitrateIdx][LayerIdx] * 1000 / (samplingRate[SamplingRateIdx])+ Padding)
+    return size
+def Frames(content):
+    pos, header=FindFrame(content)
+    pos_list=[]
+    count=0
+    try:
+        while True:
+            pos, header=FindFrame(content, pos, header)
+            pos_list.append(pos)
+            count+=1
+    except Exception: # is reached as ( pos > file size )
+        return count, pos_list
+def isAvailableToSteg(content, pos):
+    k=0
+    for i in range(0, LENGTH-2):
+        if content[pos+36+i]==content[pos+36+i+1]: pass
+        else: return False
+    return True
+def FramesAvailableToSteg(content): # returns number of frames and their posotions
+    count=0
+    available_frames=[]
+    their_headers=[]
+    pos=-1
+    pos, header=FindFrame(content)
+    while pos<len(content):
+        try:
+            pos, header=FindFrame(content, pos, header)
+        except Exception:
+            return count, available_frames, their_headers
+        if isAvailableToSteg(content, pos):
+            count+=1
+            available_frames.append(pos)
+            their_headers.append(header)
+def MaxStegSize(content):
+    num,positions,headers = FramesAvailableToSteg(content)
+    if num==0:
+        return False
+    availableList=[]
+    for frame in range(num):
+        k=0
+        while content[positions[frame]+43]==content[positions[frame]+43+k]:
+            k+=1
+        availableList.append(k)
+    return sum(availableList), availableList
+def AvailableInMp3(inputFile):
+    content=ReadFile(inputFile)
+    n, alist = MaxStegSize(content)
+    return n
+def HideFile(inputFile, fileToHide, resultFile):
+    content=list(ReadFile(inputFile))
+    secret=ReadFile(fileToHide)
+    num,positions,headers = FramesAvailableToSteg(content)
+    if num==0:
+        return False
+    bytesAvailable, availableList = MaxStegSize(content)
+    if bytesAvailable<len(secret):
+        print 'Lacks',len(secret)-bytesAvailable,'bytes'
+        return False
+    frame=0
+    while frame<len(positions):
+        mp3pos=positions[frame]
+        available=availableList[frame]
+        if available>len(secret):
+            available=len(secret)
+        if available<10: break
+        content[mp3pos+36:mp3pos+40]=SIGNATURE
+        content[mp3pos+40:mp3pos+43]=str(available).rjust(3,' ')
+        content[mp3pos+43:mp3pos+43+available]=secret[:available]
+        secret=secret[available:]
+        frame+=1
+    WriteFile(resultFile, content)
+    return True
+def RevealFile(inputFile, outputFile):
+    content=ReadFile(inputFile)
+    secret=[]
+    num, positions= Frames(content)
+    if num==0:
+        return False
+    size=None
+    for frame in range(num):
+        if content[positions[frame]+36:positions[frame]+40]==SIGNATURE:
+            size=int(content[positions[frame]+40:positions[frame]+43])
+            secret.append(content[positions[frame]+43:positions[frame]+43+size])
+    WriteFile(outputFile, secret)
+    return True
+def DelDir(dirname, create=False): # remove or create directory with content
         try:
                 for root, dirs, files in os.walk(dirname, topdown=False):
                     for name in files:
@@ -38,16 +158,16 @@ def delfile(filename): # remove file if possible
                 os.remove(filename)
         except (Exception): pass
 def clean(): # remove temporary files
-        deldir('temp')
-        deldir('temp2')
-        deldir('temp_out')
-        deldir('extracted_temp')
+        DelDir('temp')
+        DelDir('temp2')
+        DelDir('temp_out')
+        DelDir('extracted_temp')
         delfile('sound.mp3')
         delfile('extracted_sound.mp3')
         delfile('decarr.txt')
         delfile('extracted_sound.mp3.txt')
         delfile('stegsound.mp3')
-        delfile('rdmtstego.log')
+        delfile('rdstego.log')
 def openimage(foldername, number): # get image from file
         try:
                 frame = cv2.imread(u'{}/{:05d}.jpg'.format(foldername, number))
@@ -60,6 +180,7 @@ def seek(foldername, brightness_list, frame_count=KEY_LENGTH): # gets key from f
         for c in range(1,frame_count+1):
                 frame, success=openimage(foldername, c)
                 if not success:
+                        clean()
                         exit(1)
                 frame_brightness = int(numpy.sum(frame)/frame.shape[0]/frame.shape[1])
                 if (frame_brightness > brightness_list[c-1]):
@@ -109,6 +230,7 @@ def hide_key(foldername, argument, outfoldername, STEP, RESIZE_CONSTANT=1):  # h
         for c in range(1, frame_count+1):
                 frame, success = openimage(foldername, c)
                 if not success:
+                        clean()
                         exit(1)
                 frame = cv2.resize(frame, dsize=None, fx=RESIZE_CONSTANT, fy=RESIZE_CONSTANT) # video resolution can be decreased
                 if ADD_BLINKING: # whether to modify frames or not
@@ -120,6 +242,7 @@ def hide_key(foldername, argument, outfoldername, STEP, RESIZE_CONSTANT=1):  # h
                         cv2.imwrite('{}/{:05d}.jpg'.format(outfoldername, c), frame)
                 except():
                         print (u'Unable to write '+'{}/{:05d}.jpg'.format(outfoldername, c))
+                        clean()
                         exit(1)
                 ##### Show video #####
                 if SHOW_VIDEO:
@@ -136,14 +259,15 @@ def try_hide(message,key, STEP, RESIZE_RATE, out_video, password='', bytes_avail
         while len(password)<32: password+='1' # AES password for the whole file
         content='' # file content
         Brightness_list=[]
-        deldir('temp2', create=True)
-        deldir('temp_out',create=True)
+        DelDir('temp2', create=True)
+        DelDir('temp_out',create=True)
         Brightness_list = hide_key('temp', key, 'temp2', STEP, RESIZE_RATE)
         with open('rdstego.log', 'a') as f:
                 bytes_available=subprocess.call(['mp3stegz_console.exe','available','sound.mp3'],stdout=f, stderr=f)
         print(u'Bytes available (in audio): '+str(bytes_available))
         if len(os.listdir('temp'))<KEY_LENGTH:
                 print (u'Video is too short.')
+                clean()
                 exit(1)
         if Brightness_list:
                 print (u'Key is hidden. '+u'Step = '+str(STEP))
@@ -153,6 +277,7 @@ def try_hide(message,key, STEP, RESIZE_RATE, out_video, password='', bytes_avail
                 print (u'Size of key recovery data = '+str(len(content)))
         except Exception as error:
                 print (u'Unable to create file. Extraction is impossible. '+ error)
+                clean()
                 exit(1)
         print (u'Writing data to sound')
         content += SEPARATOR
@@ -167,26 +292,19 @@ def try_hide(message,key, STEP, RESIZE_RATE, out_video, password='', bytes_avail
         with open('decarr.txt','wb') as f:
                 f.write(content)
         print (u'Hiding data into audio...')
-        try:
-                with open('rdstego.log', 'a') as f:
-                        code = subprocess.call(
-                                ['mp3stegz_console.exe', 'hide','decarr.txt', 'sound.mp3','stegsound.mp3'], stdout=f, stderr=f)
-                print (u'Success')
-        except Exception as error:
-                print (u"Audio doesn't have enough free space.")
-                print (error)
+        HideFile('sound.mp3','decarr.txt','stegsound.mp3')
         print (u'Writing output video')
         with open('rdstego.log', 'a') as f:
                 code=subprocess.call(['ffmpeg','-i','stegsound.mp3','-r',str(25),'-i','temp2/%05d.jpg','-c:v','libx264','-crf',str(25),'-acodec',
                                       'copy',out_video,'-y','-hide_banner'],stdout=f, stderr=f)
         return Brightness_list
-def hide(in_video, out_video, message, STEP, RESIZE_RATE, password=''):
+def Hide(in_video, out_video, message, STEP, RESIZE_RATE, password=''):
         key=''.join(chr(random.randrange(0,255)) for i in range(KEY_LENGTH//8)) # encryption key
         while len(key)<32: key+='0'
         encryptor = Cipher(algorithms.AES(key), modes.CTR(INNER_CTR_VALUE), backend=default_backend()).encryptor()
         message = encryptor.update(message) + encryptor.finalize() # encrypting
         argument=''.join(format(ord(x), 'b').rjust(8,'0') for x in key) # key as a binary
-        deldir('temp', create=True)
+        DelDir('temp', create=True)
         with open('rdstego.log', 'a') as f: # separate frames and audio
                 code=subprocess.call(['ffmpeg','-i',in_video,'-f','mp3','sound.mp3','-r',str(25),'temp/%05d.jpg','-y','-hide_banner'],stdout=f, stderr=f)
         print (u'Opened video file')
@@ -198,26 +316,24 @@ def hide(in_video, out_video, message, STEP, RESIZE_RATE, password=''):
         print (u'Step ='+str(STEP))
         print (u'Do not forget to check quality')
         clean()
-def extract(in_video, out_file, password=''):
+def Extract(in_video, out_file, password=''):
         while len(password)<32: password+='1' # password has to be 128bit
         message=''
         print (u'Extracting data from '+ in_video+ u' into ' +out_file)
-        deldir ('extracted_temp', create = True)
+        DelDir ('extracted_temp', create = True)
         with open('rdstego.log', 'a') as f:
                 code=subprocess.call(['ffmpeg','-i',in_video,'-acodec','copy','extracted_sound.mp3','-r',str(25),'extracted_temp/%05d.jpg','-y','-hide_banner'],stdout=f, stderr=f)
         if code!=0: # separate frames and audio
                 clean()
                 return False
         brightness_list=[]
-        with open('rdstego.log', 'a') as f:
-                code = subprocess.call(
-                        ['mp3stegz_console.exe', 'reveal', 'extracted_sound.mp3'], stdout=f, stderr=f)
-        if code!=0:
+        success=RevealFile('extracted_sound.mp3','decarr.txt')
+        if not success:
                 print ('Audio is broken =(')
                 clean()
                 return False
         try:
-                with open('extracted_sound.mp3.txt', "rb") as f:
+                with open('decarr.txt', "rb") as f:
                         content=f.read()
                         decryptor = Cipher(algorithms.AES(password), modes.CTR(OVERALL_CTR_VALUE), backend=default_backend()).decryptor()
                         content = decryptor.update(content) + decryptor.finalize()
@@ -243,14 +359,13 @@ def extract(in_video, out_file, password=''):
                 f.write(message)
         clean()
         return True
-def available (in_video, message_file): # check available space
-        deldir('temp', create=True)
+def Available (in_video, message_file): # check available space
+        DelDir('temp', create=True)
         try:
                 with open('rdstego.log', 'a') as f: # separate frames and audio
                         subprocess.call(['ffmpeg','-i',in_video,'-f','mp3','sound.mp3','-r',str(25),'temp/%05d.jpg','-y','-hide_banner'],stdout=f, stderr=f)
                 print (u'Opened video')
-                with open('rdstego.log', 'a') as f:
-                        bytes_available=subprocess.call(['mp3stegz_console.exe','available','sound.mp3'],stdout=f, stderr=f)
+                bytes_available=AvailableInMp3('sound.mp3')
                 print(u'Bytes available (audio): '+str(bytes_available))
                 frame_count=len(os.listdir('temp'))
                 print(u'Max key length = '+str(frame_count)+u' bits (not more than 128bit)')
@@ -324,9 +439,11 @@ def main():
                         if output_video_file[-4:].lower() != '.mp4':# add extension if required
                                 output_video_file += '.mp4'
                 # hide
-                try: hide(args.input_video_file, output_video_file, message, initial_step, resize_rate, password)
+                try:
+                        Hide(args.input_video_file, output_video_file, message, initial_step, resize_rate, password)
                 except Exception as error:
                         print(error)
+                        clean()
                         exit(1)
                 print (u'SUCCESS. You can use '+output_video_file)
         # Extraction
@@ -334,9 +451,10 @@ def main():
                 password = getpass.getpass('Type password: ')
                 with open(args.extracted_file,'w'): pass
                 try:
-                        secret=extract(args.input_video_file, args.extracted_file,password)
+                        secret=Extract(args.input_video_file, args.extracted_file,password)
                 except:
                         print (u'No hidden data found')
+                        clean()
                         exit(0)
                 print (u'Check file '+args.extracted_file)
                 print (u'It may contain your data if password was correct')
@@ -348,7 +466,7 @@ def main():
                         print (''.join([chr(ord(x)) for x in message]))
         elif args.command == 'available':
                 print (u'Checking free space')
-                result, success = available(args.input_video_file,args.message_file)
+                result, success = Available(args.input_video_file,args.message_file)
                 if success:
                         print (u'Enough space. '+str(result)+u' bytes available after hide')
                 elif result==-1:
